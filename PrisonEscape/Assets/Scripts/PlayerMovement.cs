91a9542d28325c7117c5909c58f8c7c05190d9c8
Jacob450 <jacob676763@gmail.com>
2023-11-12 16:14:23 -0500

    11/12/2023
    Fixed some animations

--- PrisonEscape/Assets/Scripts/PlayerMovement.cs
+++ PrisonEscape/Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D player;
     public float movementSpeed;
     private float inputHorizontal, inputVertical;
-   
+    private Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         move();
-        
+        playerAnimate();
     }
-
+    private void playerAnimate()
+    {
+       if(inputVertical == 1)
+        {
+            animator.SetBool("MovingUp", true);
+            animator.SetBool("MovingDown", false);
+        }
+        else if(inputVertical == -1) 
+        { 
+            animator.SetBool("MovingDown", true);
+            animator.SetBool("MovingUp", false);
+        }
+        else
+        {
+            animator.SetBool("MovingUp", false);
+            animator.SetBool("MovingDown", false);
+        }
+    }
     
        
     private void move()
     {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         inputVertical = Input.GetAxisRaw("Vertical");
-
+        Debug.Log(inputVertical);
         player.velocity = new Vector2(movementSpeed * inputHorizontal, movementSpeed * inputVertical); ;
 
         GameManager.setPlayerPosX(transform.position.x);

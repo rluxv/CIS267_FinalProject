8e9857f6988e8ad151f2f787a7b749223c260fcf
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-11-14 19:23:51 -0500

    Main Menu work

--- PrisonEscape/Assets/Scripts/MainMenu.cs
+++ PrisonEscape/Assets/Scripts/MainMenu.cs
@@ -7,17 +7,21 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject ArrowText;
+    [SerializeField] private GameObject MainMenuButtons;
+    [SerializeField] private GameObject LoadMenuButtons;
     private TMP_Text ArrowTextTMP;
     private int selected;
     private bool ctrlrHold;
-
     private int textAnimPos;
+
+    private bool saveMenuOpen;
 
     AudioSource audioData;
     public void Start()
     {
         selected = 0;
         ctrlrHold = false;
+        saveMenuOpen = false;
         audioData = GetComponent<AudioSource>();
         ArrowTextTMP = ArrowText.GetComponent<TMP_Text>();
     }
@@ -36,6 +40,25 @@ public class MainMenu : MonoBehaviour
         controllerInput();
         input(false);
     }
+
+    public void openLoadSaveMenu()
+    {
+        MainMenuButtons.SetActive(false);
+        LoadMenuButtons.SetActive(true);
+        saveMenuOpen = true;
+        selected = 0;
+        ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 100, 0);
+    }
+
+    public void openMainMenu()
+    {
+        LoadMenuButtons.SetActive(false);
+        MainMenuButtons.SetActive(true);
+        saveMenuOpen = false;
+        selected = 0;
+        ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 100, 0);
+    }
+
     private void controllerInput()
     {
         if(Input.GetAxis("Vertical") == 1 || Input.GetAxis("Vertical") == -1)
@@ -58,34 +81,103 @@ public class MainMenu : MonoBehaviour
 
     private void input(bool ctrlPress)
     {
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.W) || ctrlPress == true) 
+        if (Input.GetKeyDown(KeyCode.S) || (ctrlPress && Input.GetAxis("Vertical") == -1))
         {
-            Debug.Log("S or W key pressed");
-            if(selected == 0)
+            if (selected == 0)
             {
                 selected = 1;
-                Debug.Log("selected " + selected); 
                 playAudio();
                 ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0);
             }
             else if (selected == 1)
             {
+                selected = 2;
+                playAudio();
+                ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -100, 0);
+            }
+            else if (selected == 2)
+            {
+                if (saveMenuOpen)
+                {
+                    selected = 3;
+                    playAudio();
+                    ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -200, 0);
+                }
+                else
+                {
+                    selected = 0;
+                    playAudio();
+                    ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 100, 0);
+                }
+            }
+            else if (selected == 3)
+            {
                 selected = 0;
-                Debug.Log("selected " + selected);
                 playAudio();
                 ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 100, 0);
-                
             }
         }
-        if(Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("AButton"))
+        else if (Input.GetKeyDown(KeyCode.W) || (ctrlPress && Input.GetAxis("Vertical") == 1))
         {
-            if(selected == 0)
+            if (selected == 0)
+            {
+                if (saveMenuOpen == true)
+                {
+                    selected = 3;
+                    playAudio();
+                    ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -200, 0);
+                }
+                else
+                {
+                    selected = 2;
+                    playAudio();
+                    ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -100, 0);
+                }
+            }
+            else if (selected == 1)
+            {
+                selected = 0;
+                playAudio();
+                ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 100, 0);
+            }
+            else if (selected == 2)
+            {
+                selected = 1;
+                playAudio();
+                ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0);
+            }
+            else if (selected == 3)
+            {
+                selected = 2;
+                playAudio();
+                ArrowText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -100, 0);
+            }
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("AButton")) && saveMenuOpen == false)
+        {
+            playAudio();
+            // 0 = start game 1 = show save menu 2 = quit
+            if (selected == 0)
             {
                 startGame();
             }
-            else if(selected == 1)
+            else if (selected == 1)
+            {
+                openLoadSaveMenu();
+            }
+            else if (selected == 2)
             {
                 quitGame();
+            }
+        }
+
+        if (saveMenuOpen)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("BButton"))
+            {
+                openMainMenu();
+                playAudio();
             }
         }
     }

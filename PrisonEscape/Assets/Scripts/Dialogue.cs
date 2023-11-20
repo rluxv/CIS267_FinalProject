43356ec3d63fb4193f8effb1ac94cdd95f2e8ca9
Jacob450 <jacob676763@gmail.com>
2023-11-20 15:13:29 -0500

    messing with dilalogue options

--- PrisonEscape/Assets/Scripts/Dialogue.cs
+++ PrisonEscape/Assets/Scripts/Dialogue.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
+
 public class Dialogue : MonoBehaviour
 {
     private PlayerDetector detector;
     public GameObject detectorObject;
+    public GameObject highlight;
+    public float offset;
     
     public TMP_Text screen;
+    public bool doesBranch;
+    
     public string[] text;
     public string initialText;
     public string[] choices;
     private string parent;
-    int pos;
-    public bool doesBranch;
-   
+    private int pos;
+    private float inputVertical;
+    float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +29,13 @@ public class Dialogue : MonoBehaviour
         pos = 0;
         
         parent = transform.name;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(!doesBranch)
         {
             dialogue();
@@ -35,7 +44,11 @@ public class Dialogue : MonoBehaviour
         if (doesBranch)
         {
             choice();
+            moveHighlight();
+            inputVertical = Input.GetAxisRaw("Vertical");
         }
+
+        Debug.Log(inputVertical);
     }
 
     private void choice()
@@ -46,7 +59,7 @@ public class Dialogue : MonoBehaviour
             screen.text =  parent + "- " + initialText +"\n"+ choices[0] + "\n" + choices[1] + "\n" + choices[2] + "\n" + choices[3];
         }
 
-
+       
     }
     
     private void dialogue()
@@ -67,5 +80,20 @@ public class Dialogue : MonoBehaviour
         }
     }
 
-    
+    private void moveHighlight()
+    {
+        
+        timer -= Time.deltaTime;
+        if (inputVertical == 1 && timer < 0)
+        {
+            highlight.transform.position = new Vector2(highlight.transform.position.x, highlight.transform.position.y + offset);
+            timer = 1;
+        }
+        if (inputVertical == -1 && timer < 0)
+        {
+            highlight.transform.position = new Vector2(highlight.transform.position.x, highlight.transform.position.y - offset);
+            timer = 1;
+        }
+        Debug.Log(timer);
+    }
 }

a0e0f702d7fe31d1ba27f5126794c3f8693e67d5
Ben Macleod <benstreasurebox@gmail.com>
2023-11-22 21:40:32 -0500

    feat: inventory root, started

--- PrisonEscape/Assets/Scripts/Core/Inventory/Inventory.cs
+++ PrisonEscape/Assets/Scripts/Core/Inventory/Inventory.cs
@@ -9,8 +9,57 @@ public class Inventory
     // Inventory size is number of items they can hold. Default = 10 items.
     public int inventorySize;
 
+    // Inventory Items
+    private List<InventoryItem> items;
+
     public Inventory()
     {
         inventorySize = Config.DEFAULT_INVENTORY_SIZE;
+        items = new List<InventoryItem>();
+    }
+
+    public void AddItem(InventoryItem item)
+    {
+        // Check if we have the item.
+        if (HasItem(item))
+        {
+            int? itemIndex = GetItemIndex(item);
+
+            /**
+             * Update the item amount if the item
+             * is stackable.
+             */
+        }
+        else
+        {
+            // Add the new items to the list.
+            items.Add(item);
+        }
+    }
+
+    public bool HasItem(InventoryItem _item)
+    {
+        foreach (InventoryItem item in items) 
+        {
+            if (item.itemId.Equals(item.itemId))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Nullable<int> GetItemIndex(InventoryItem _item)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].itemId.Equals(_item.itemId))
+            {
+                return i;
+            }
+        }
+
+        return null;
     }
 }
